5e74b3f8127650ca89ea0c3b241f922d3acbaafd
sonicxml <sonicxml@gmail.com>
2012-08-04 21:29:14 -0400

    Hopefully finally fix up toroplus

--- toroplus/device-vendor-blobs.mk
+++ toroplus/device-vendor-blobs.mk
@@ -32,6 +32,27 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/toroplus/proprietary/libWVStreamControlAPI_L1.so:system/vendor/lib/libWVStreamControlAPI_L1.so \
     vendor/samsung/toroplus/proprietary/libdrmwvmplugin.so:system/vendor/lib/drm/libdrmwvmplugin.so \
     vendor/samsung/toroplus/proprietary/lib_gsd4t.so:system/lib/lib_gsd4t.so \
+    vendor/samsung/toro/proprietary/pvrsrvctl:system/vendor/bin/pvrsrvctl \
+    vendor/samsung/toro/proprietary/sirfgps.conf:system/vendor/etc/sirfgps.conf \
+    vendor/samsung/toro/proprietary/bcm4330.hcd:system/vendor/firmware/bcm4330.hcd \
+    vendor/samsung/toro/proprietary/ducati-m3.bin:system/vendor/firmware/ducati-m3.bin \
+    vendor/samsung/toro/proprietary/libpn544_fw.so:system/vendor/firmware/libpn544_fw.so \
+    vendor/samsung/toro/proprietary/libEGL_POWERVR_SGX540_120.so:system/vendor/lib/egl/libEGL_POWERVR_SGX540_120.so \
+    vendor/samsung/toro/proprietary/libGLESv1_CM_POWERVR_SGX540_120.so:system/vendor/lib/egl/libGLESv1_CM_POWERVR_SGX540_120.so \
+    vendor/samsung/toro/proprietary/libGLESv2_POWERVR_SGX540_120.so:system/vendor/lib/egl/libGLESv2_POWERVR_SGX540_120.so \
+    vendor/samsung/toro/proprietary/gps.omap4.so:system/vendor/lib/hw/gps.omap4.so \
+    vendor/samsung/toro/proprietary/gralloc.omap4.so:system/vendor/lib/hw/gralloc.omap4.so \
+    vendor/samsung/toro/proprietary/libglslcompiler.so:system/vendor/lib/libglslcompiler.so \
+    vendor/samsung/toro/proprietary/libIMGegl.so:system/vendor/lib/libIMGegl.so \
+    vendor/samsung/toro/proprietary/libims.so:system/vendor/lib/libims.so \
+    vendor/samsung/toro/proprietary/libims_jni.so:system/vendor/lib/libims_jni.so \
+    vendor/samsung/toro/proprietary/libinvensense_mpl.so:system/vendor/lib/libinvensense_mpl.so \
+    vendor/samsung/toro/proprietary/libpvr2d.so:system/vendor/lib/libpvr2d.so \
+    vendor/samsung/toro/proprietary/libpvrANDROID_WSEGL.so:system/vendor/lib/libpvrANDROID_WSEGL.so \
+    vendor/samsung/toro/proprietary/libPVRScopeServices.so:system/vendor/lib/libPVRScopeServices.so \
+    vendor/samsung/toro/proprietary/libsrv_init.so:system/vendor/lib/libsrv_init.so \
+    vendor/samsung/toro/proprietary/libsrv_um.so:system/vendor/lib/libsrv_um.so \
+    vendor/samsung/toro/proprietary/libusc.so:system/vendor/lib/libusc.so
 
 # All the apks necessary for toroplus
 PRODUCT_PACKAGES += \
