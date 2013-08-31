2174d177a1005b677f3016737a6bbe4198c6fba0
nuclearmistake <nuclearmistake@gmail.com>
2013-08-31 17:03:01 -0400

    Toroplus had better support for APN stuff and stuff before updating... sprint != good people. toroplus == fustercluck.
    Change-Id: If61fde19163a5836d3c86f330151844e9e0b1306

--- toroplus/device-toroplus.mk
+++ toroplus/device-toroplus.mk
@@ -11,3 +11,5 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+$(call inherit-product-if-exists, vendor/samsung/toroplus/device-vendor-blobs.mk)
