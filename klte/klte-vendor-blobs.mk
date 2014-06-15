7f345a3cb9d535db9204bdeafef020251b34706c
nuclearmistake <nuclearmistake@gmail.com>
2014-06-15 09:18:21 -0400

    Off-device drm blobs
    Change-Id: I6ed4a1ba82bc6380e628bc73f870698d3b7e3a6f

--- klte/klte-vendor-blobs.mk
+++ klte/klte-vendor-blobs.mk
@@ -312,6 +312,12 @@ PRODUCT_COPY_FILES += \
 	vendor/samsung/klte/proprietary/vendor/lib/libWVStreamControlAPI_L1.so:system/vendor/lib/libWVStreamControlAPI_L1.so \
 	vendor/samsung/klte/proprietary/lib/libwvm.so:system/lib/libwvm.so \
 	vendor/samsung/klte/proprietary/lib/drm/libdrmwvmplugin.so:system/lib/drm/libdrmwvmplugin.so \
+	vendor/samsung/klte/proprietary/lib/drm/libfwdlockengine.so:system/lib/drm/libfwdlockengine.so \
+	vendor/samsung/klte/proprietary/lib/drm/libomaplugin.so:system/lib/drm/libomaplugin.so \
+	vendor/samsung/klte/proprietary/lib/drm/libplayreadyplugin.so:system/lib/drm/libplayreadyplugin.so \
+	vendor/samsung/klte/proprietary/lib/drm/libprgenericplugin.so:system/lib/drm/libprgenericplugin.so \
+	vendor/samsung/klte/proprietary/lib/drm/libsdrmplugin.so:system/lib/drm/libsdrmplugin.so \
+	vendor/samsung/klte/proprietary/vendor/lib/libdrmdiag.so:system/vendor/lib/libdrmdiag.so \
 	vendor/samsung/klte/proprietary/vendor/lib/libdrmfs.so:system/vendor/lib/libdrmfs.so \
 	vendor/samsung/klte/proprietary/vendor/lib/libdrmtime.so:system/vendor/lib/libdrmtime.so \
 	vendor/samsung/klte/proprietary/vendor/lib/liboemcrypto.so:system/vendor/lib/liboemcrypto.so \
