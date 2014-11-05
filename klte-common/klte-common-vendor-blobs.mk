65e1a1356f7eafa03337c6472e7a05e6bc3633df
Ethan Chen <intervigil@gmail.com>
2014-11-05 11:53:30 -0800

    klte-common: Add missing Widevine library
    * Samsung package version: G900FXXU1ANG2_G900FOXA1ANG3_BTU
    
    Conflicts:
    
    	klte-common/klte-common-vendor-blobs.mk
    
    Change-Id: I6acaca5b4a93ebc3eec5bf3fbe56ed6395d3044e

--- klte-common/klte-common-vendor-blobs.mk
+++ klte-common/klte-common-vendor-blobs.mk
@@ -217,6 +217,9 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/klte-common/proprietary/vendor/lib/libQSEEComAPI.so:system/vendor/lib/libQSEEComAPI.so \
     vendor/samsung/klte-common/proprietary/vendor/lib/librpmb.so:system/vendor/lib/librpmb.so \
     vendor/samsung/klte-common/proprietary/vendor/lib/libssd.so:system/vendor/lib/libssd.so \
+    vendor/samsung/klte-common/proprietary/vendor/lib/libwvdrm_L1.so:system/vendor/lib/libwvdrm_L1.so \
+    vendor/samsung/klte-common/proprietary/vendor/lib/libWVStreamControlAPI_L1.so:system/vendor/lib/libWVStreamControlAPI_L1.so \
+    vendor/samsung/klte-common/proprietary/vendor/lib/mediadrm/libwvdrmengine.so:system/vendor/lib/mediadrm/libwvdrmengine.so \
     vendor/samsung/klte-common/proprietary/vendor/lib/egl/eglsubAndroid.so:system/vendor/lib/egl/eglsubAndroid.so \
     vendor/samsung/klte-common/proprietary/vendor/lib/egl/libEGL_adreno.so:system/vendor/lib/egl/libEGL_adreno.so \
     vendor/samsung/klte-common/proprietary/vendor/lib/egl/libGLESv1_CM_adreno.so:system/vendor/lib/egl/libGLESv1_CM_adreno.so \
