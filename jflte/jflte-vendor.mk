38b30931968a84c7853ff22c84e2ea66592ed3c6
DHOMD <DHO@dho.im>
2014-11-16 15:14:46 -0500

    Reconcile JF related blobs for reworked jf trees
    from cm
    
    Change-Id: Ia8dd666bde918ea0e5c095994eb0cde1cd17dd6e

--- jflte/jflte-vendor.mk
+++ jflte/jflte-vendor.mk
@@ -16,7 +16,6 @@
 
 # Pick up overlay for features that depend on non-open-source files
 PRODUCT_PACKAGES += \
-    libtime_genoff \
-    QuickBoot
+    libtime_genoff
 
 $(call inherit-product, vendor/samsung/jflte/jflte-vendor-blobs.mk)
