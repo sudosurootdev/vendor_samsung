49996ab3e616d81897360b1a3aaf3917c1b09b98
Ozzy2403 <oskar.kratochvil@gmx.de>
2015-02-05 23:55:49 +0100

    awesome kernel needs apk to get configured
    Change-Id: Ibd569ee6dbbf2e4dd0d70200062de1d49f6be118

--- klte-common/Android.mk
+++ klte-common/Android.mk
@@ -19,6 +19,26 @@ LOCAL_PATH := $(call my-dir)
 ifneq ($(filter klte kltedv kltespr klteusc kltevzw,$(TARGET_DEVICE)),)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := KTweaker
+LOCAL_MODULE_OWNER := samsung
+LOCAL_SRC_FILES := proprietary/app/KTweaker.apk
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_SUFFIX := .apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_CERTIFICATE := PRESIGNED
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := KTmonitor
+LOCAL_MODULE_OWNER := samsung
+LOCAL_SRC_FILES := proprietary/app/KTmonitor.apk
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_SUFFIX := .apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_CERTIFICATE := PRESIGNED
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := TimeService
 LOCAL_MODULE_OWNER := samsung
 LOCAL_SRC_FILES := proprietary/app/TimeService.apk
--- klte-common/klte-common-vendor.mk
+++ klte-common/klte-common-vendor.mk
@@ -16,6 +16,8 @@
 
 # Pick up overlay for features that depend on non-open-source files
 PRODUCT_PACKAGES += \
+    KTweaker \
+    KTmonitor \
     TimeService \
     libHevcSwDecoder \
     libmm-abl \
